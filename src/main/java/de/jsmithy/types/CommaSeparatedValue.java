fbea8c652fca83738c8d31b22d6974bc97d31e72
Erik Lotz <elo@jsmithy.de>
2017-03-20 21:43:38 +0100

    Enrich behavior of type 'CommaSeparatedValue'.

--- src/main/java/de/jsmithy/types/CommaSeparatedValue.java
+++ src/main/java/de/jsmithy/types/CommaSeparatedValue.java
@@ -1,5 +1,7 @@
 package de.jsmithy.types;
 
+import java.util.*;
+
 /**
  * 
  * @author Erik Lotz
@@ -8,10 +10,65 @@ package de.jsmithy.types;
  */
 public final class CommaSeparatedValue {
 
+	private List<String> values;
+	private String value;
+
 	private CommaSeparatedValue(String aValue) {
+		value = aValue;
+		values = splitIntoValues(aValue);
+	}
+
+	List<String> splitIntoValues(String aValue) {
+		List<String> separatedValues = new ArrayList<>();
+		if (!aValue.isEmpty()) {
+			String[] result = aValue.split(",");
+			separatedValues = Arrays.asList(result);
+		}
+
+		return separatedValues;
 	}
 
 	public static CommaSeparatedValue newInstance(String aValue) {
-		return new CommaSeparatedValue(aValue);
+		String csvString = (aValue == null) ? "" : aValue.trim();
+
+		return new CommaSeparatedValue(csvString);
+	}
+
+	public int size() {
+		return values.size();
+	}
+
+	public boolean isEmpty() {
+		return getValue().isEmpty();
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " '" + getValue() + "'";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CommaSeparatedValue other = (CommaSeparatedValue) obj;
+
+		return getValue().equals(other.getValue());
 	}
 }
--- src/test/java/de/jsmithy/types/CommaSeparatedValueTest.java
+++ src/test/java/de/jsmithy/types/CommaSeparatedValueTest.java
@@ -22,28 +22,135 @@ public class CommaSeparatedValueTest {
 
 	@Test
 	public void testNewInstance_pass_empty_string_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("");
 		assertNotNull("Instance creation is not correct!", sut);
 	}
 
 	@Test
 	public void testNewInstance_pass_blank_string_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("   ");
 		assertNotNull("Instance creation is not correct!", sut);
 	}
 
 	@Test
 	public void testNewInstance_pass_null_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance(null);
 		assertNotNull("Instance creation is not correct!", sut);
 	}
 
 	@Test
-	public void testCountValues_with_zero_value() {
+	public void testSize_equals_zero_for_empty_string_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("");
+		int expected = 0;
+
+		int actual = sut.size();
+
+		assertEquals("Size of instance has not been correctly calculated!", expected, actual);
 	}
 
 	@Test
-	public void testCountValues_with_one_value() {
+	public void testSize_equals_zero_for_blank_string_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("   ");
+		int expected = 0;
+
+		int actual = sut.size();
+
+		assertEquals("Size of instance has not been correctly calculated!", expected, actual);
 	}
 
 	@Test
-	public void testCountValues_with_many_values() {
+	public void testSize_equals_zero_for_null_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance(null);
+		int expected = 0;
+
+		int actual = sut.size();
+
+		assertEquals("Size of instance has not been correctly calculated!", expected, actual);
+	}
+
+	@Test
+	public void testSize_with_one_value() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("abc");
+		int expected = 1;
+
+		int actual = sut.size();
+
+		assertEquals("Size of instance has not been correctly calculated!", expected, actual);
+	}
+
+	@Test
+	public void testSize_with_many_values() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("abc,def,ghi,jkl");
+		int expected = 4;
+
+		int actual = sut.size();
+
+		assertEquals("Size of instance has not been correctly calculated!", expected, actual);
+	}
+
+	@Test
+	public void testIsEmpty_true() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("");
+
+		boolean result = sut.isEmpty();
+
+		assertTrue("Predicate 'isEmpty' is not correct!", result);
+	}
+
+	@Test
+	public void testIsEmpty_false() {
+		boolean result = sut.isEmpty();
+
+		assertFalse("Predicate 'isEmpty' is not correct!", result);
+	}
+
+	@Test
+	public void testToString() {
+		String expected = "CommaSeparatedValue 'hello,world'";
+
+		String actual = sut.toString();
+
+		assertEquals("String representation is not correct!", expected, actual);
+	}
+
+	@Test
+	public void testEquals_same_values() {
+		CommaSeparatedValue sut1 = CommaSeparatedValue.newInstance("abc,def");
+		CommaSeparatedValue sut2 = CommaSeparatedValue.newInstance("abc,def");
+
+		assertEquals("Equality is not checked correctly!", sut1, sut2);
+	}
+
+	@Test
+	public void testEquals_same_instance() {
+		CommaSeparatedValue sut1 = CommaSeparatedValue.newInstance("abc,def");
+		CommaSeparatedValue sut2 = sut1;
+
+		assertEquals("Equality is not checked correctly!", sut1, sut2);
+	}
+
+	@Test
+	public void testEquals_compare_with_null() {
+		CommaSeparatedValue sut1 = CommaSeparatedValue.newInstance("abc,def");
+		CommaSeparatedValue sut2 = null;
+
+		assertNotEquals("Equality is not checked correctly!", sut1, sut2);
+	}
+
+	@Test
+	public void testEquals_compare_with_other_object() {
+		CommaSeparatedValue sut = CommaSeparatedValue.newInstance("abc,def");
+		String string = "abc,def";
+
+		assertNotEquals("Equality is not checked correctly!", sut, string);
+	}
+
+	@Test
+	public void testHashCode() {
+		int expected = 2137655895;
+
+		int actual = sut.hashCode();
+
+		assertEquals("Hash code is not correct!", expected, actual);
 	}
 }
